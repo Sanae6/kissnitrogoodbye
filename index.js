5152844806291c4cad16a24a1e910d8eb940d3f9
Sanae <atmatm6000@outlook.com>
2022-01-23 17:59:51 -0600

    Kick and warn users instead of banning them

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const config = require("./config.json");
 const bot = new Eris(config.token);
 
 bot.on("messageCreate", async msg => {
-    if (msg.member === undefined) return;
     const isSus = await sdp.checkMessage(msg.content);
     if (isSus) {
-        console.log(`Banned ${msg.member.username}#${msg.member.discriminator}`)
-        await msg.member.ban(1, config.banReason);
+        console.log(`Removed message from ${msg.author.username}#${msg.author.discriminator}`);
+        await msg.delete(config.reason);
+
+        // Only dm them once at most, avoid repeats.
+        if (msg.member === undefined) return;
+        const invite = await msg.channel.createInvite({
+            unique: true,
+            maxUses: 1,
+            reason: config.reason
+        });
+        const privChannel = await msg.author.getDMChannel();
+        await privChannel.createMessage("Kicked for sending a scam link. Reset your password, enable two factor authentication on your account." +
+            `You can rejoin using this invite: https://discord.gg/${invite.code}`);
+        await msg.member.kick(config.reason);
     }
-})
+});
 
 bot.connect();
